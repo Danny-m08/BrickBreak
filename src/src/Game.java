c550fb9202d06b6f3f0799b66464a6c0ae987b61
Commit 1 <drm16g@my.fsu.edu>
2019-04-10 12:39:18 -0400

    Initial Commit

--- src/src/Game.java
+++ src/src/Game.java
@@ -1,8 +1,41 @@
-public class Game {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Game extends JFrame{
 
     // This class is the main Game loop
+    JMenuBar menuBar;
+
+	public Game(){
+		super("Brick Break");
+		super.setSize(800,650);
+
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menuBar = new JMenuBar();
+		JMenu newGame = new JMenu("New Game");
+		JMenuItem startGame = new JMenuItem("Start New Game");
+
+
+		startGame.setMnemonic('s');
+
+		newGame.add(startGame);
+		menuBar.add(newGame);
+
+		startGame.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				JInternalFrame gameFrame = new JInternalFrame("Welcome");
+				gameFrame.setVisible(true);
+				System.out.println("Starting new game!");
+			}
+		});
+
+		super.setJMenuBar(menuBar);
+	}
+
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Game brickBreak = new Game();
+        brickBreak.setVisible(true);
     }
 }
